bdb15a743c9c3461061fd4a5c977c8e0f9d90610
Fernando Natalino <fnatalino@sorcero.com>
2026-02-24 01:39:45 +0000

    chore: update environmentMatchGlobs

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
     globals: true,
     setupFiles: ['./vitest.setup.ts'],
     include: ['src/**/*.test.ts', 'src/**/*.test.tsx'],
-    environmentMatchGlobs: [['src/components/**/*.test.tsx', 'jsdom']],
   },
   resolve: {
     alias: {
